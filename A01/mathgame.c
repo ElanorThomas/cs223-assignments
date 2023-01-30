b9191618492ab710ccd5792d6e836840cb977598
Ellie Christine Jolin Thomas <ethomas@goldengate.cs.brynmawr.edu>
2023-01-30 12:17:32 -0500

    Assignment 1 Commit 1

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -1,11 +1,30 @@
 /***************************************************
  * mathgame.c
- * Author: 
+ * Author:Elanor Thomas 
  * Implements a math game
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+printf("Welcome to math game! How many rounds do you want to play?");
+    int numQuestions;
+    int numCorrect = 0;
+    scanf("%d", &numQuestions);
+    int i, op1, op2, givenAns;
+    for( i = 0 ; i < numQuestions; i++ ) {
+        op1 = rand()%10;
+        op2 = rand()%10;
+        printf("%d + %d = ? ", op1, op2);
+        scanf("%d", &givenAns);
+        if (givenAns == op1 + op2){
+            printf("Correct!\n");
+            numCorrect++;
+        } else {
+            printf("Incorrect ;(\n");
+        }
+   }
+   printf("You answered %d / %d correctly.\n", numCorrect, numQuestions);
   return 0;
 }
